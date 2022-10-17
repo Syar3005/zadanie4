a28049da942550974c9feea3aa8cf0d57c03eeba
Syar3005 <113889600+Syar3005@users.noreply.github.com>
2022-10-17 15:12:32 +0300

    Add files via upload

---
+++ задание4/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Введите координаты белого коня от 1 до 8");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты черного коня от 1 до 8");
+int c = Convert.ToInt32(Console.ReadLine());
+int d = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты, куда пойдет белый конь");
+int e = Convert.ToInt32(Console.ReadLine());
+int f = Convert.ToInt32(Console.ReadLine());
+
+if ((a + 1 == e && b + 2 == f) ||
+    (a - 1 == e && b + 2 == f) ||
+    (a + 1 == e && b - 2 == f) ||
+    (a - 1 == e && b - 2 == f) ||
+    (a + 2 == e && b + 1 == f) ||
+    (a - 2 == e && b + 1 == f) ||
+    (a + 2 == e && b - 1 == f) ||
+    (a - 2 == e && b - 1 == f))
+{
+    if ((a + 1 == e && b + 2 == f) ||
+    (c - 1 == e && d + 2 == f) ||
+    (c + 1 == e && d - 2 == f) ||
+    (c - 1 == e && d - 2 == f) ||
+    (c + 2 == e && d + 1 == f) ||
+    (c - 2 == e && d + 1 == f) ||
+    (c + 2 == e && d - 1 == f) ||
+    (c - 2 == e && d - 1 == f))
+    {
+        Console.WriteLine("Белый конь может сходить, но будет под ударом черного коня");
+    }
+    else
+        Console.WriteLine("Белый конь может сходить туда, и не будет под ударом черного коня");
+}
+else
+{
+    Console.WriteLine("Белый конь туда не ходит за 1 ход");
+}
